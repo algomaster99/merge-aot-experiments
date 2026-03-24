def89d5f0a86fbbbadbc646628844dbb84e81616
Aman Sharma <mannu.poski10@gmail.com>
2026-03-24 13:27:46 +0100

    Try out all modules of PDFBox

--- pdfbox-experiment/orchestrate-combine-3.sh
+++ pdfbox-experiment/orchestrate-combine-3.sh
@@ -29,9 +29,9 @@ rm -f tree.aot
 java -Xlog:aot \
   -XX:AOTMode=merge \
   -XX:AOTCache=pdfbox/tools/cache.aot \
-  -XX:AOTMergeInputs="pdfbox-deps/pdfbox-jbig2/cache.aot:pdfbox-deps/apache-commons-io/cache.aot" \
+  -XX:AOTMergeInputs="pdfbox-deps/pdfbox-jbig2/cache.aot:pdfbox-deps/apache-commons-io/cache.aot:pdfbox/io/cache.aot:pdfbox/fontbox/cache.aot:pdfbox/xmpbox/cache.aot:pdfbox/pdfbox/cache.aot:pdfbox/preflight/cache.aot:pdfbox/tools/cache.aot:pdfbox/examples/cache.aot" \
   -XX:AOTCacheOutput=tree.aot \
-  -cp "pdfbox-deps/pdfbox-jbig2/target/classes/:pdfbox-deps/apache-commons-io/target/classes/" \
+  -cp "pdfbox-deps/pdfbox-jbig2/target/classes/:pdfbox-deps/apache-commons-io/target/classes/:pdfbox/app/target/pdfbox-app-3.0.7.jar" \
   -version
 
 test -f tree.aot
